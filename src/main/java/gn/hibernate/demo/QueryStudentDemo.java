a048296b73ada88e85a46add56f0393cd97f3ccd
n-gregory-a <ovonely@gmail.com>
2019-11-01 14:56:49 +0700

    Hibernate Tutorial: - add QueryStudentDemo class which create a session factory, then session and then querying Student table for all students and then only students who have last name Doe

---
+++ src/main/java/gn/hibernate/demo/QueryStudentDemo.java
@@ -0,0 +1,57 @@
+package gn.hibernate.demo;
+
+import gn.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class QueryStudentDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+
+            // start a transaction
+            session.beginTransaction();
+
+            // query students
+            List<Student> students = session.createQuery("from Student").getResultList();
+
+            // display the students
+            displayStudents(students);
+
+            // query students: lastName="Doe"
+            students = session.createQuery("from Student  s where s.lastName='Doe'").getResultList();
+
+            // display the students
+            System.out.println("\n\nStudents who have last name of Doe");
+            displayStudents(students);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+        } finally {
+            factory.close();
+        }
+    }
+
+    private static void displayStudents(List<Student> students) {
+        for (Student tempStudent : students) {
+            System.out.println(tempStudent);
+        }
+    }
+}
