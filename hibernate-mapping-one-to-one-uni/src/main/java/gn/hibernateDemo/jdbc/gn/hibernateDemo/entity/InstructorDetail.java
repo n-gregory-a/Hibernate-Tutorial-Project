772dc39a6df566f43687f70c801da32f5f93524d
n-gregory-a <ovonely@gmail.com>
2019-11-13 21:42:00 +0700

    Hibernate Tutorial: - configure hibernate.cfg.xml - create InstructorDetail entity class

---
+++ hibernate-mapping-one-to-one-uni/src/main/java/gn/hibernateDemo/jdbc/gn/hibernateDemo/entity/InstructorDetail.java
@@ -0,0 +1,72 @@
+package gn.hibernateDemo.jdbc.gn.hibernateDemo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+    // annotate the class as an entity and map to db table
+
+    // define the fields
+    // annotate the fields with db column names
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youTubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    // create constructors
+
+    public InstructorDetail() {
+    }
+
+    public InstructorDetail(String youTubeChannel, String hobby) {
+        this.youTubeChannel = youTubeChannel;
+        this.hobby = hobby;
+    }
+
+    // generate getter/setter methods
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYouTubeChannel() {
+        return youTubeChannel;
+    }
+
+    public void setYouTubeChannel(String youTubeChannel) {
+        this.youTubeChannel = youTubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+
+    // generate toString() method
+
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youTubeChannel='" + youTubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
