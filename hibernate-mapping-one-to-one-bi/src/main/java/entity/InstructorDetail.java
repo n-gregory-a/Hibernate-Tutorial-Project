be45148840128fb047a6275f562ca4724a299535
n-gregory-a <ovonely@gmail.com>
2019-11-14 22:16:08 +0700

    Hibernate Tutorial: - delete only instructor detail

--- hibernate-mapping-one-to-one-bi/src/main/java/entity/InstructorDetail.java
+++ hibernate-mapping-one-to-one-bi/src/main/java/entity/InstructorDetail.java
@@ -22,7 +22,8 @@ public class InstructorDetail {
     private String hobby;
 
     // add new field for instructor
-    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "instructorDetail",
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private Instructor instructor;
 
     // create constructors
