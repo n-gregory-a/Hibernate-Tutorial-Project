b2c6da474af8aaae3fdefd7c5321188ab3cd5710
n-gregory-a <ovonely@gmail.com>
2019-11-16 17:56:11 +0700

    Hibernate Tutorial: - add EagerLazyDemo class that get instructor by id and retrieve courses for this instructor - add parameter fetch = FetchType.EAGER to @OneToMAny annotation for eager loading - add hibernate.cfg.xml

---
+++ hb-eager-vs-lazy-demo/src/main/java/gn/hibernateDemo/EagerLazyDemo.java
@@ -0,0 +1,51 @@
+package gn.hibernateDemo;
+
+import gn.hibernateDemo.entity.Course;
+import gn.hibernateDemo.entity.Instructor;
+import gn.hibernateDemo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class EagerLazyDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the instructor from db
+            int id = 1;
+            Instructor instructor = session.get(Instructor.class, id);
+
+            System.out.println("luv2code: Instructor: " + instructor);
+
+            // retrieve courses from db
+            System.out.println("luv2code: Courses: " + instructor.getCourses());
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("luv2code: Done!");
+
+        } finally {
+            // add clean up code
+            session.close();
+
+            factory.close();
+        }
+    }
+}
--- hb-eager-vs-lazy-demo/src/main/java/gn/hibernateDemo/entity/Instructor.java
+++ hb-eager-vs-lazy-demo/src/main/java/gn/hibernateDemo/entity/Instructor.java
@@ -32,7 +32,7 @@ import java.util.List;
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
 
-    @OneToMany(mappedBy = "instructor",
+    @OneToMany(mappedBy = "instructor", fetch = FetchType.EAGER,
             cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private List<Course> courses;
 
