5f4379135389a2102b81ad9cc4064873def3c1bf
n-gregory-a <ovonely@gmail.com>
2019-11-17 20:16:15 +0700

    Hibernate Tutorial: - create GetCoursesAndReviewsDemo class retrieving from db demonstration

---
+++ hb-04-one-to-many-uni/src/main/java/gn/hibernateDemo/GetCoursesAndReviewsDemo.java
@@ -0,0 +1,54 @@
+package gn.hibernateDemo;
+
+import gn.hibernateDemo.entity.Course;
+import gn.hibernateDemo.entity.Instructor;
+import gn.hibernateDemo.entity.InstructorDetail;
+import gn.hibernateDemo.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetCoursesAndReviewsDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the course
+            int id = 10;
+            Course course = session.get(Course.class, id);
+
+            // print the course
+            System.out.println(course);
+
+            // print the course reviews
+            System.out.println(course.getReviews());
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+        } finally {
+            // add clean up code
+            session.close();
+
+            factory.close();
+        }
+    }
+}
