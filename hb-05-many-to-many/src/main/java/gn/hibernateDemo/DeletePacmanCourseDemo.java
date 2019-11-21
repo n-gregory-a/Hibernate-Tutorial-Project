e4d46b02dd394747fa759bf6320edd7f5ed8ada5
n-gregory-a <ovonely@gmail.com>
2019-11-21 21:55:31 +0700

    Hibernate Tutorial: - create new app to delete courses of student

---
+++ hb-05-many-to-many/src/main/java/gn/hibernateDemo/DeletePacmanCourseDemo.java
@@ -0,0 +1,50 @@
+package gn.hibernateDemo;
+
+import gn.hibernateDemo.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeletePacmanCourseDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the pacman form db
+            int courseId = 10;
+            Course course = session.get(Course.class, courseId);
+
+            // delete the course
+            System.out.println("Deleting course: " + course);
+            session.delete(course);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+        } finally {
+            // add clean up code
+            session.close();
+
+            factory.close();
+        }
+    }
+}
