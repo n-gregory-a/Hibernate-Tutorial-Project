122a9692beea44524beeb20205668505e94407b9
Grigory <ovonely@gmail.com>
2019-11-29 23:45:32 +0700

    starting new module web-customer-tracker

---
+++ web-customer-tracker/src/main/Java/gn/testdb/TestDbServlet.java
@@ -0,0 +1,17 @@
+package gn.testdb;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet (HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+}
